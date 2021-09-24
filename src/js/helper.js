32f40732cfa2e2eb41a0aae2ce0490be7ee09780
hermawangan <hermawangan39@gmail.com>
2021-09-24 14:56:06 +0700

    fraction error fix

--- src/js/helper.js
+++ src/js/helper.js
@@ -63,3 +63,35 @@ export const sendJSON = async function (url, uploadData) {
   }
 };
 */
+
+export const numberToFraction = function (amount) {
+  // This is a whole number and doesn't need modification.
+  if (parseFloat(amount) === parseInt(amount)) {
+    return amount;
+  }
+  // Next 12 lines are cribbed from https://stackoverflow.com/a/23575406.
+  const gcd = function (a, b) {
+    if (b < 0.0000001) {
+      return a;
+    }
+    return gcd(b, Math.floor(a % b));
+  };
+  const len = amount.toString().length - 2;
+  let denominator = Math.pow(10, len);
+  let numerator = amount * denominator;
+  var divisor = gcd(numerator, denominator);
+  numerator /= divisor;
+  denominator /= divisor;
+  let base = 0;
+  // In a scenario like 3/2, convert to 1 1/2
+  // by pulling out the base number and reducing the numerator.
+  if (numerator > denominator) {
+    base = Math.floor(numerator / denominator);
+    numerator -= base * denominator;
+  }
+  amount = Math.floor(numerator) + '/' + Math.floor(denominator);
+  if (base) {
+    amount = base + ' ' + amount;
+  }
+  return amount;
+};
--- src/js/views/recipeViews.js
+++ src/js/views/recipeViews.js
@@ -2,7 +2,7 @@ import View from './view.js';
 
 // import icons from '../img/icons.svg'; //! parcel 1
 import icons from 'url:../../img/icons.svg'; //! parcel 2
-import { Fraction } from 'fractional';
+import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -128,7 +128,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
      </svg>
      <div class="recipe__quantity">${
-       ing.quantity ? new Fraction(ing.quantity).toString() : ''
+       ing.quantity ? numberToFraction(ing.quantity).toString() : ''
      }</div>
      <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
